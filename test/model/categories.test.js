a9315ec9ff2006adae6166ad4dea20cddda84ea5
Ingrzs <ing.ramirozs@gmail.com>
2022-05-16 23:19:27 -0500

    test actualizado

--- test/model/categories.test.js
+++ test/model/categories.test.js
@@ -1,15 +1,14 @@
 
 const categories = require("./../../app/model/Categories")
-const db = require("./../../app/lib/DB");
-db.connect();
+
 
 describe(" test", () => {
     test("Test Order", async () => {
         
         const consulta = await categories.getAllCategories();
         expect(consulta[0].name).toBe("Coral reefs");
+        
     });
-     
     });
     
     
